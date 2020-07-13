8369611187bb9a87c6c482c690582b0f1477ba30
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-07-12 23:34:11 -0300

    allow rendering for wrappers too

--- layout.ex
+++ layout.ex
@@ -94,33 +94,55 @@ defmodule Layout do
     def pad(<<kc::binary-size(1)>>), do: "  " <> kc <> "  "
     def pad(key), do: String.pad_leading(key, 5, " ")
 
-    def layer_template(name, left_keys, right_keys) do
+    def render(name, left_keys, right_keys, template \\ &layer_template/5) do
       row_mapper = fn keys ->
-        {headers, keys} =
-          Enum.map(keys, fn
-            lt(layer, "KC_" <> kc) ->
-              {
-                "#define KC_LS#{layer - 7}_#{kc} LT(ASETNIOP#{layer - 7}, KC_#{kc})\n",
-                "LS#{layer - 7}_#{kc}"
-              }
+        Enum.map(keys, fn
+          lt(layer, "KC_" <> kc) ->
+            {
+              "#define KC_LS#{layer - 7}_#{kc} LT(ASETNIOP#{layer - 7}, KC_#{kc})\n",
+              "LS#{layer - 7}_#{kc}"
+            }
 
-            key("KC_TRANSPARENT") ->
-              ""
+          key("KC_TRANSPARENT") ->
+            ""
 
-            key("KC_" <> key) ->
-              key
-          end)
-          |> Enum.map(fn
-            {header, kc} -> {header, pad(kc)}
-            other -> {"", pad(other)}
-          end)
-          |> Enum.unzip()
-
-        {Enum.join(headers), Enum.join(keys, ",")}
+          key("KC_" <> key) ->
+            key
+        end)
+        |> Enum.map(fn
+          {header, kc} -> {header, kc}
+          other -> {"", other}
+        end)
+        |> Enum.unzip()
       end
 
       {left_headers, left_row} = row_mapper.(left_keys)
       {right_headers, right_row} = row_mapper.(right_keys)
+
+      template.(name, left_headers, left_row, right_headers, right_row)
+    end
+
+
+    def wrapper_template(name, _, left_row, _, right_row) do
+      left_row = left_row |> Enum.map(&pad(&1)) |> Enum.join(",")
+      right_row = right_row |> Enum.map(&pad(&1)) |> Enum.join(",")
+
+      """
+      //                              |-----+-----+-----+-----|
+      #define ______L_#{ name }______  #{        left_row    }
+      //                              |-----+-----+-----+-----|
+
+      //                              |-----+-----+-----+-----|
+      #define ______R_#{ name }______  #{        right_row   }
+      //                              |-----+-----+-----+-----|
+      """
+    end
+
+    def layer_template(name, left_headers, left_row, right_headers, right_row) do
+      left_row = left_row |> Enum.map(&pad(&1)) |> Enum.join(",")
+      right_row = right_row |> Enum.map(&pad(&1)) |> Enum.join(",")
+      left_headers = left_headers |> Enum.map(&pad(&1)) |> Enum.join()
+      right_headers = right_headers |> Enum.map(&pad(&1)) |> Enum.join()
 
       """
       #{left_headers}#{right_headers}  [#{name}] = LAYOUT_kc(
--- run.exs
+++ run.exs
@@ -4,10 +4,11 @@ layouts = Layout.load("keymap.c") |> hd()
 Enum.each(0..8, fn i ->
   layer = Layout.at(layouts, i)
 
-  Layout.Renderer.layer_template(
+  Layout.Renderer.render(
     "ASETNIOP#{i}",
     Enum.map(15..18, fn index -> Layout.at(layer, index) end),
-    Enum.map(53..56, fn index -> Layout.at(layer, index) end)
+    Enum.map(53..56, fn index -> Layout.at(layer, index) end),
+    &Layout.Renderer.wrapper_template/5
   )
   |> IO.puts()
 end)
